85a5374f78282ca402e10cf44fe37e23c79a6af6
Andrew Rainboldt <arainboldt@protonmail.com>
2021-07-06 18:07:23 +0300

    Update get_checks.R

--- R/get_checks.R
+++ R/get_checks.R
@@ -166,8 +166,7 @@ check_design <- function(data, id, block, item, choice, aggregate = FALSE,
       }
     if (any(sapply(test, getElement, "pw_same"))) {
       if (!nonbibd) {
-        warning("Each pairwise comparison between items must occur the same amount
-  of times for each id.")
+        warning("Each pairwise comparison between items must occur the same amount of times for each id.")
       }
     }
   }
